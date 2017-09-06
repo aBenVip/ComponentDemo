1ed565809f9ed55c20973cce4052a351ccc24ec7
aben <abenvip@163.com>
2017-09-06 17:12:16 +0800

    Gradle集成封装测试，尚未完成

--- app/src/main/java/com/dgcredit/componentdemo/MainActivity.java
+++ app/src/main/java/com/dgcredit/componentdemo/MainActivity.java
@@ -1,8 +1,13 @@
 package com.dgcredit.componentdemo;
 
+import android.graphics.Canvas;
+import android.graphics.Paint;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextPaint;
 import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.alibaba.android.arouter.launcher.ARouter;
 
@@ -12,6 +17,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView textView = (TextView) findViewById(R.id.cehsi);
+        textView.setOnClickListener(v -> {
+            Toast.makeText(this, "Lalallalalla ", Toast.LENGTH_SHORT).show();
+        });
     }
 
     public void moduleA(View view) {
